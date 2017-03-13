f517684aa224609325473b5a46557b9b49ffaa4b
winonecheng <f74036328@mail.ncku.edu.tw>
2017-03-13 16:18:48 +0800

    optimize

--- mosquito/models.py
+++ mosquito/models.py
@@ -4,7 +4,12 @@ from django.db import models
 class User(models.Model):
 	state = models.IntegerField(default=0)
 	uid = models.CharField(max_length=200)
-	temperature = models.CharField(max_length=200)
+	temperature = models.FloatField(default=0)
 	address = models.CharField(max_length=200)
-	latitude = models.CharField(max_length=200)
-	longitude = models.CharField(max_length=200)
+	latitude = models.FloatField(default=0)
+	longitude = models.FloatField(default=0)
+	report_date = models.DateTimeField()
+
+	def __str__(self):
+		return self.uid
+
--- mosquito/views.py
+++ mosquito/views.py
@@ -1,32 +1,49 @@
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+
 
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, LocationMessage
 
 from .models import User
+import re
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
      
 def create_row_in_db(id):
-    user = User(uid=id)
+    user = User(uid=id, report_date=timezone.now())
     user.save()
 
-def update_user_data(id, data):
-    user = User.objects.filter(uid=id).reverse()[0]
+def update_user_data(id, message):
+    user = User.objects.filter(uid=id).order_by('-report_date')[0]
     if user.state == 0:
-        user.temperature = data
+        user.temperature = message
         user.state = 1
         user.save()
+
     elif user.state == 1:
-        user.address = data
+        user.address = message.address
+        user.latitude = message.latitude
+        user.longitude = message.longitude
         user.state = 2
         user.save()
+
     else:
         print("update data error!!")
+
+def handle_input_of_temperature(id, message):
+    pat = r'^\s*(\d+\.*\d*)\s*'u'(度)'
+    str = message.text
+    match = re.search(pat,str)
+    if match:
+        update_user_data(id, float(match.group(1)))
+        return 1
+    else:
+        return 0
 
 @csrf_exempt
 def callback(request):
@@ -43,38 +60,47 @@ def callback(request):
 
         for event in events:
             if isinstance(event, MessageEvent):
-                if isinstance(event.message, TextMessage):
-                    profile = line_bot_api.get_profile(event.source.user_id)
-                    reply_text = ""
+                profile = line_bot_api.get_profile(event.source.user_id)
+                try:
+                    state = User.objects.filter(uid=profile.user_id).order_by('-report_date')[0].state
+                except:
+                    state = -1                
 
+                if isinstance(event.message, TextMessage):
                     if "回報" in event.message.text:
                         print(profile.user_id+"要回報")
                         create_row_in_db(profile.user_id)
-                        reply_text = "嗨，"+profile.display_name+"\n請問你現在的體溫?"
+                        state = 0
 
                     else:
-                        state = -1
-                        try:
-                            state = User.objects.filter(uid=profile.user_id).reverse()[0].state
-                            
-                        except:
-                            reply_text = "嗨，"+profile.display_name+"\n你要回報嗎?"
-
                         if state == 0:
                             print(profile.user_id+"輸入體溫")
-                            update_user_data(profile.user_id, event.message.text)
-                            reply_text = "請問你的所在地?"
-                        elif state == 1:
-                            print(profile.user_id+"輸入地點")
-                            update_user_data(profile.user_id, event.message.text)
-                            reply_text = "回報成功！"
-                        elif state == 2:
-                        	reply_text = "你要再次回報嗎?"
+                            if(handle_input_of_temperature(profile.user_id, event.message)):
+                                state = 1
 
-                    line_bot_api.reply_message(
-                        event.reply_token,
-                        TextSendMessage(text=reply_text)
-                    )
+                elif isinstance(event.message, LocationMessage):
+                    if state == 1:
+                        print(profile.user_id+"輸入地點")
+                        update_user_data(profile.user_id, event.message)
+                        state = 2
+                else:
+                    pass
+
+                """ handle replay message """
+                reply_text = ""
+                if state == 0:
+                    reply_text = "請問你現在的體溫？\n(ex：27.5度)"
+                elif state == 1:
+                    reply_text = "請問你的所在地？\n(使用「位置訊息」功能)"
+                elif state == 2:
+                    reply_text = "回報成功！\n"
+                else:
+                    reply_text = "嗨，"+profile.display_name+"\n你要回報嗎？\n(輸入「回報」 開始回報)"
+
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=reply_text)
+                )
 
         return HttpResponse()
     else:
