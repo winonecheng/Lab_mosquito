eb6a1c2ab0fe75733fda06b7e9ee40c0a264366a
winonecheng <f74036328@mail.ncku.edu.tw>
2017-03-24 00:23:36 +0800

    fix

--- mosquito_test/settings.py
+++ mosquito_test/settings.py
@@ -141,6 +141,3 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
